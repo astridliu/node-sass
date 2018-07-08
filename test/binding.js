cec10ba40526fc647bb9b354557497195949c1b1
xzyfer <xzyfer@gmail.com>
2018-07-09 01:04:51 +1000

    WIP

--- test/binding.js
+++ test/binding.js
@@ -100,14 +100,14 @@ describe('binding', function() {
 
     describe('with an unsupported runtime', function() {
       beforeEach(function() {
-        Object.defineProperty(process.versions, 'modules', {
+        Object.defineProperty(process.versions, 'napi', {
           value: 'baz',
         });
       });
 
       afterEach(function() {
-        Object.defineProperty(process.versions, 'modules', {
-          value: 51,
+        Object.defineProperty(process.versions, 'napi', {
+          value: 3,
         });
       });
 
